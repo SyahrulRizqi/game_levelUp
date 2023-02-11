ccc400ca1e428ff362f2da850ce647992ecd36c4
SyahrulRizqi <syahrulemon996@gmail.com>
2023-02-11 18:34:11 +0700

    coba

---
+++ src/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    system("cls");
+
+    cout << "test" << endl;
+    cout << "test lagi" << endl;
+    cout << "Lagi dehh" << endl;
+    cout << "udah dehh :)" << endl;
+    cout << "iya deh" << endl;
+    return 0;
+}
